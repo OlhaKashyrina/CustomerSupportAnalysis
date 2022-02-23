2611703d64e9e2ce7388a9cbdc86c3c571cd4470
helga <eleferia3021@gmail.com>
2022-02-23 16:35:32 +0200

    Minor refactoring

--- src/main/analytics/Analytics.java
+++ src/main/analytics/Analytics.java
@@ -3,33 +3,38 @@ package main.analytics;
 import main.analytics.exceptions.NoDataToAnalyseException;
 import main.analytics.parsers.InputParser;
 import main.analytics.parsers.OutputGenerator;
-
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Analytics {
 
-    public static final String tryAgain = "Please, try again.";
+    public static final String TRY_AGAIN = "Please, try again.";
+    public static final String WELCOME = "Welcome to Customer Support Analytics!";
+    public static final String INSTRUCTION = "Enter the path to a file with input data or enter \"exit\" to quit:";
+    public static final String EXIT = "exit";
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Customer Support Analytics!");
+        System.out.println(WELCOME);
         boolean running = true;
         while (running)
         {
-            System.out.println("Enter the path to a file with input data or enter \"exit\" to quit:");
+            System.out.println(INSTRUCTION);
             String path = new Scanner(System.in).nextLine();
-            if (path.equals("exit"))
+            if (path.equals(EXIT))
                 running = false;
             else
                 try {
-                    OutputGenerator og = new OutputGenerator();
-                    String resultPath = og.writeReport(InputParser.parseFile(path), path);
-                    System.out.println("Done! File with the result can be found here: " + resultPath);
+                    System.out.println("Done! File with the result can be found here: " + analyseData(path));
                 } catch (IOException e) {
-                    System.out.println(tryAgain);
+                    System.out.println(TRY_AGAIN);
                 } catch (NoDataToAnalyseException e) {
-                    System.out.println(e + tryAgain);
+                    System.out.println(e + TRY_AGAIN);
                 }
         }
+    }
+
+    private static String analyseData(String path) throws IOException, NoDataToAnalyseException {
+        OutputGenerator og = new OutputGenerator();
+        return og.writeReport(InputParser.parseFile(path), path);
     }
 }
--- src/main/analytics/parsers/InputParser.java
+++ src/main/analytics/parsers/InputParser.java
@@ -3,7 +3,6 @@ package main.analytics.parsers;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
